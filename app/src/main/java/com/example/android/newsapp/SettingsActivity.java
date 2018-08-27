633b3d4903881082760880cf0e1bdc2a689050dd
numerative <michael.hathi@gmail.com>
2018-08-27 16:26:50 +0530

    Add Settings Activity and Fragment and PreferenceScreen and ListPreference(s)

--- app/src/main/java/com/example/android/newsapp/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsapp/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.newsapp;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -9,5 +11,19 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public static class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            return false;
+        }
     }
 }
